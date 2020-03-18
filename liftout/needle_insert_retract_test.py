f94553ee76ed7fa67ab3dccbe466bf345a9d1264
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-03-18 15:23:46 +1100

    Save logfile from needle_insert_retract_test.py

--- liftout/needle_insert_retract_test.py
+++ liftout/needle_insert_retract_test.py
@@ -1,4 +1,18 @@
+import logging
 import time
+
+
+def configure_logging(log_filename='logfile.log', log_level=logging.DEBUG):
+    """Log to the terminal and to file simultaneously."""
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=log_level,
+        # Multiple handlers can be added to your logging configuration.
+        # By default log messages are appended to the file if it exists already
+        handlers=[
+            logging.FileHandler(log_filename),
+            logging.StreamHandler(),
+        ])
 
 
 def initialize(ip_address='10.0.0.1'):
@@ -23,34 +37,35 @@ def retract_properly(microscope, park_position):
 def main():
     microscope = initialize()
     park_position = microscope.specimen.manipulator.current_position
-    print('Parked position is at: {}'.format(park_position))
-    print('Retracting needle...')
+    logging.info('Parked position is at: {}'.format(park_position))
+    logging.info('Retracting needle...')
     microscope.specimen.manipulator.retract()
     retract_position_original = microscope.specimen.manipulator.current_position
-    print('Needle retracted successfully')
+    logging.info('Needle retracted successfully')
     time.sleep(0.5)
-    print('Inserting needle...')
+    logging.info('Inserting needle...')
     microscope.specimen.manipulator.insert()
-    print('Needle inserted successfully')
-    print('Original parked position: {}'.format(park_position))
+    logging.info('Needle inserted successfully')
+    logging.info('Original parked position: {}'.format(park_position))
     current_position = microscope.specimen.manipulator.current_position
-    print('Current parked position: {}'.format(current_position))
+    logging.info('Current parked position: {}'.format(current_position))
     # User interaction
-    print('Please move the needle manually')
+    logging.info('Please move the needle manually')
     while True:    # infinite loop
         user_input = input("Have you finished moving the needle manually? yes/no\n")
         if user_input == "yes":
             retract_properly(microscope, park_position)
             retract_position = microscope.specimen.manipulator.current_position
-            print('Original retracted position: {}'.format(retract_position_original))
-            print('Current retracted position: {}'.format(retract_position))
-            print('Finished.')
+            logging.info('Original retracted position: {}'.format(retract_position_original))
+            logging.info('Current retracted position: {}'.format(retract_position))
+            logging.info('Finished.')
             break  # stops the loop
 
 
 if __name__ == "__main__":
     user_input = input("Is the needle calibrated and in the park position? yes/no\n")
     if user_input == 'yes':
+        configure_logging(log_filename='needle_insert_remove_logfiles.log')
         main()
     else:
         print('Ok, cancelling program.')
