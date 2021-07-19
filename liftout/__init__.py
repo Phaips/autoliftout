72a6143d40d046c2c3a447f675d07433038aef62
Daviddierickx <David.Dierickx1@monash.edu>
2021-07-19 15:57:19 +1000

    import refactor of unused functions in subfolder 'old_functions'

--- liftout/__init__.py
+++ liftout/__init__.py
@@ -3,13 +3,13 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from liftout.acquire import *
-from liftout.align import *
-from liftout.calibration import *
-from liftout.display import *
+from liftout.old_functions.acquire import *
+from liftout.old_functions.align import *
+from liftout.old_functions.calibration import *
+from liftout.old_functions.display import *
 from liftout.main import *
-from liftout.milling import *
-from liftout.needle import *
-from liftout.stage_movement import *
+from liftout.old_functions.milling import *
+from liftout.old_functions.needle import *
+from liftout.old_functions.stage_movement import *
 from liftout.user_input import *
 from liftout.detection import *
