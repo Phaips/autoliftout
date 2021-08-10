fe9ee949a5fbd3b7a6018b3df311a808d147bf28
Patrick Cleeve <prcle2@student.monash.edu>
2021-08-10 15:45:15 +1000

    sample class  save / load is working

--- liftout/fibsem/sample.py
+++ liftout/fibsem/sample.py
@@ -2,7 +2,7 @@
 import glob
 from enum import Enum
 from pprint import pprint
-
+import random
 import matplotlib.pyplot as plt
 import yaml
 from liftout.detection import utils
@@ -18,27 +18,54 @@ class SampleStatus(Enum):
     Finished = 6
 
 class FakeStagePosition():
-    """ Mock stage position because dont have access to autoscript"""
-    def __init__(self) -> None:
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.r = 0
-        self.t = 0
+    """ Mock StagePosition because dont have access to autoscript"""
+    def __init__(self, x=0, y=0, z=0, r=0, t=0) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+        self.r = r
+        self.t = t
+
+    def __repr__(self) -> str:
+        
+        return f"x={self.x}, y={self.y}, z={self.z}, r={self.r}, t={self.t}"
 
 class Sample:
-    def __init__(self):
+    def __init__(self, data_path, sample_no):
         self.landing_coordinates = FakeStagePosition() # StagePosition()
         self.lamella_coordinates = FakeStagePosition() # StagePosition()
         self.landing_ref_images = list()
         self.lamella_ref_images = list()
         self.status = NotImplemented
-        self.data_path = "/Users/patrickcleeve/Documents/university/bio/demarco/autoliftout/liftout/gui/log/run/20210804.151912/"
+        self.data_path = data_path
         self.timestamp = self.data_path.split("/")[-2]
+        self.sample_no = sample_no
 
+    def setup_yaml_file(self):
+        # check if yaml file already exists for this timestamp..
+        yaml_file =glob.glob(self.data_path + "*sample.yaml")
+
+        # TODO: investigate if there will there ever be more than 1 yaml file?
+        if yaml_file:
+            # read and open existing yaml file
+            with open(yaml_file[0], 'r') as f:
+                sample_yaml = yaml.safe_load(f)
+
+        else:
+            # create new yaml file
+            sample_yaml = {
+                        "timestamp": self.timestamp,
+                        "data_path": self.data_path, 
+                        "sample": {}
+                        }
+        return sample_yaml
     def save_data(self):
+        """ Save the lamella and landing coordinates, and reference to data path"""
         
+        # check if yaml file already exists for this timestamp..
+        sample_yaml = self.setup_yaml_file()
 
+        # format coordinate data for saving        
         lamella_coordinates_dict = {
             "x": self.lamella_coordinates.x,
             "y": self.lamella_coordinates.y,
@@ -58,129 +85,137 @@ class Sample:
         # save stage position to yml file
         save_dict = {
             "timestamp": self.timestamp,
+            "sample_no": self.sample_no,
             "lamella_coordinates": lamella_coordinates_dict,
             "landing_coordinates": landing_coordinates_dict,
-            "data_path": self.data_path}
+            # "data_path": self.data_path
+            }
+
+        # format dictionary
+        sample_yaml["sample"][self.sample_no] = save_dict
 
         # should we save the images separately? or just use previously saved?
         with open(f"{self.data_path}sample.yaml", "w") as outfile:
-            yaml.dump(save_dict, outfile)
+            yaml.dump(sample_yaml, outfile)
 
-
-
-        return NotImplemented
-
-    def load_data_from_file(self, fname):
+    def load_data_from_file(self, fname=None):
+        
+        if fname is None:
+            fname = f"{sample.data_path}sample.yaml"
 
         # load yaml file
-
         with open(fname, 'r') as f:
-            load_dict = yaml.safe_load(f)
-
-        pprint(load_dict)
-
-        # TODO: improve this
-        ref_landing_lowres_eb = self.data_path + "img/01_ref_landing_low_res_eb.tif"
-        ref_landing_highres_eb = self.data_path + "img/01_ref_landing_high_res_eb.tif"
-        ref_landing_lowres_ib = self.data_path + "img/01_ref_landing_low_res_ib.tif"
-        ref_landing_highres_ib = self.data_path + "img/01_ref_landing_high_res_ib.tif"
-        ref_lamella_lowres_eb = self.data_path + "img/01_ref_lamella_low_res_eb.tif"
-        ref_lamella_highres_eb = self.data_path + "img/01_ref_lamella_high_res_eb.tif"
-        ref_lamella_lowres_ib = self.data_path + "img/01_ref_lamella_low_res_ib.tif"
-        ref_lamella_highres_ib = self.data_path + "img/01_ref_lamella_high_res_ib.tif"
-
-        ref_landing_fnames = [
-            ref_landing_lowres_eb,
-            ref_landing_highres_eb,
-            ref_landing_lowres_ib,
-            ref_landing_highres_ib
-        ]
-
-        ref_lamella_fnames = [
-            ref_lamella_lowres_eb,
-            ref_lamella_highres_eb,
-            ref_lamella_lowres_ib,
-            ref_lamella_highres_ib
-        ]
-
+            sample_yaml = yaml.safe_load(f)
         
-        for fname in ref_landing_fnames:
+        sample_dict = sample_yaml["sample"][self.sample_no]
+
+        # load stage positions from yaml
+        self.lamella_coordinates = FakeStagePosition(x=sample_dict["lamella_coordinates"]["x"],
+                                                    y=sample_dict["lamella_coordinates"]["y"],
+                                                    z=sample_dict["lamella_coordinates"]["z"],
+                                                    r=sample_dict["lamella_coordinates"]["r"],
+                                                    t=sample_dict["lamella_coordinates"]["t"])
+
+
+        self.landing_coordinates = FakeStagePosition(x=sample_dict["landing_coordinates"]["x"],
+                                                    y=sample_dict["landing_coordinates"]["y"],
+                                                    z=sample_dict["landing_coordinates"]["z"],
+                                                    r=sample_dict["landing_coordinates"]["r"],
+                                                    t=sample_dict["landing_coordinates"]["t"])
+        
+        # TODO: improve this
+        # load images from disk
+        sample_no = sample_dict["sample_no"]
+        ref_landing_lowres_eb = self.data_path + f"img/{sample_no:02d}_ref_landing_low_res_eb.tif"
+        ref_landing_highres_eb = self.data_path + f"img/{sample_no:02d}_ref_landing_high_res_eb.tif"
+        ref_landing_lowres_ib = self.data_path + f"img/{sample_no:02d}_ref_landing_low_res_ib.tif"
+        ref_landing_highres_ib = self.data_path + f"img/{sample_no:02d}_ref_landing_high_res_ib.tif"
+        ref_lamella_lowres_eb = self.data_path + f"img/{sample_no:02d}_ref_lamella_low_res_eb.tif"
+        ref_lamella_highres_eb = self.data_path + f"img/{sample_no:02d}_ref_lamella_high_res_eb.tif"
+        ref_lamella_lowres_ib = self.data_path + f"img/{sample_no:02d}_ref_lamella_low_res_ib.tif"
+        ref_lamella_highres_ib = self.data_path + f"img/{sample_no:02d}_ref_lamella_high_res_ib.tif"
+
+
+        # load the adorned images and format        
+        for fname in [ref_landing_lowres_eb, ref_landing_highres_eb, ref_landing_lowres_ib, ref_landing_highres_ib]:
+            
+            #img = AdornedImage.load(fname)
             img = utils.load_image_from_file(fname)
 
-            plt.title(fname)
-            plt.imshow(img)
-            plt.show()
             self.landing_ref_images.append(img) #TODO: change to AdornedImage
 
-        for fname in ref_lamella_fnames:
-            img = utils.load_image_from_file(fname)
+        for fname in [ref_lamella_lowres_eb, ref_lamella_highres_eb, ref_lamella_lowres_ib, ref_lamella_highres_ib]:
             
-            plt.title(fname)
-            plt.imshow(img)
-            plt.show()
+            #img = AdornedImage.load(fname)
+            img = utils.load_image_from_file(fname)
+
             self.lamella_ref_images.append(img) #TODO: change to AdornedImage
 
+        
+        # for img in self.landing_ref_images + self.lamella_ref_images:
+        #     plt.title(fname.split("/")[-1])
+        #     plt.imshow(img)
+        #     plt.show()
 
 
-        # print(self.landing_ref_images)
-        # print(self.lamella_ref_images)
-        # load stage positions from yaml
+    def get_sample_data(self):
+        """Return the sample data formatted for liftout from the specificed data_path. """
+        self.load_data_from_file()
 
-        # load images from disk
+        return (self.lamella_coordinates, self.landing_coordinates, self.lamella_ref_images, self.landing_ref_images)
 
-        # format for lifout
-
-        return NotImplemented
-
+        
 if __name__ == "__main__":
 
-    sample = Sample()
+    data_path = "/Users/patrickcleeve/Documents/university/bio/demarco/autoliftout/liftout/gui/log/run/20210804.151912/"
     
-    sample.lamella_coordinates.x = 1
-    sample.lamella_coordinates.y = 1
-    sample.lamella_coordinates.z = 1
-    sample.lamella_coordinates.r = 1
-    sample.lamella_coordinates.t = 1
+    sample = Sample(data_path=data_path, sample_no=random.randint(1, 2))
+    
+    # create fake data
+    sample.lamella_coordinates.x = sample.sample_no
+    sample.lamella_coordinates.y = sample.sample_no
+    sample.lamella_coordinates.z = sample.sample_no
+    sample.lamella_coordinates.r = sample.sample_no
+    sample.lamella_coordinates.t = sample.sample_no
     
     sample.save_data()
-    print(sample)
 
-    fname = f"{sample.data_path}sample.yaml"
-    sample.load_data_from_file(fname=fname)
+    data_path = "/Users/patrickcleeve/Documents/university/bio/demarco/autoliftout/liftout/gui/log/run/20210804.151912/"
+    
+    sample_new = Sample(data_path=data_path, sample_no=sample.sample_no)
 
-
-# timestamp="20210804.151912"
-
-# self.landing_coordinates = [StagePosition(x=-0.0033364968,y=-0.003270375,z=0.0039950765,t=0.75048248,r=4.0317333)]
-
-# self.lamella_coordinates = [StagePosition(x=0.0028992212,y=-0.0033259583,z=0.0039267467,t=0.43632805,r=4.0318009)]
-# # self.zipped_coordinates = None
-# self.zipped_coordinates = list(zip(self.lamella_coordinates, self.landing_coordinates))
-
-# original_landing_images = ((f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_landing_low_res_eb.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_landing_high_res_eb.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_landing_low_res_eb_ib.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_landing_high_res_eb_ib.tif'))
-# self.original_landing_images = list()
-# for image in original_landing_images:
-#     self.original_landing_images.append(AdornedImage.load(image))
-
-
-# original_trench_images= ((f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_lamella_low_res_eb.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_lamella_high_res_eb.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_lamella_low_res_eb_ib.tif',
-#                             f'C:/Users/Admin/Github/autoliftout/liftout/gui/log/run/{timestamp}/img/01_ref_lamella_high_res_eb_ib.tif'))
-# self.original_trench_images = list()
-# for image in original_trench_images:
-#     self.original_trench_images.append(AdornedImage.load(image))
-
-# self.original_landing_images = [self.original_landing_images]
-# self.original_trench_images = [self.original_trench_images]
+    lamella_coordinates, land_coordinates, ref_land_imgs, ref_lamella_imgs = sample_new.get_sample_data()
+    
+    print(f"Sample {sample_new.sample_no:02d}:",  sample_new.timestamp)
+    print("Land: ", land_coordinates)
+    print("Lamella: ", lamella_coordinates)
+    print(len(ref_land_imgs), len(ref_lamella_imgs))
 
 
 
-# Sample
-# - lamella_coordinates
-# - lamella_ref_images
-# - landing_coordinates
-# - landing_ref_images
+# folder structure
+# log/run/timestamp/
+#   sample.yaml
+#   img/
+
+# the names of the images are fixed, wiht a sample_no prefix.
+# so we dont need to save the fnames, just the timestamp...
+
+# Sample file structure
+# sample.yaml:
+#   timestamp:
+#   data_path:
+#   sample:
+#       1: Sample()
+#           timestamp: float
+#           sample_no: int
+#           lamella_coordinates: StagePosition()
+#           landing_coordinates: StagePosition()
+#       2: Sample()
+#       3: Sample()
+#       ...
+
+
+# TODO:
+# might be worth adding .current_sample to Liftout
+# then we can change the status and display to user?
